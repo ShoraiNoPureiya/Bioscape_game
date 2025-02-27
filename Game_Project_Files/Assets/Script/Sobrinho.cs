d96a7b4835e7addd3eb009438450baf0dcdef40b
LorenaCastroS <168767812+LorenaCastroS@users.noreply.github.com>
2025-02-27 15:55:15 -0300

    27/02
    -Animação do sobrinho
    -Programação do sobrinho
    -Sobrinho adicionado como prefab

---
+++ Game_Project_Files/Assets/Script/Sobrinho.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sobrinho : MonoBehaviour
+{
+
+    public GameObject Player;
+    public Animator _SobrinhoAnimator;
+    public float _InteractionDistance = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector2.Distance(transform.position, Player.transform.position) <= _InteractionDistance) //When the player is near and press the E button the talking animation starts
+        {
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+                _SobrinhoAnimator.SetTrigger("Talking");
+            }
+        }
+    }
+}
+
+
