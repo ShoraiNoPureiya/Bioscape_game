4c15d46d4425eff602f176fc20936969b77524d2
Murilo-Matsuora <169314047+Murilo-Matsuora@users.noreply.github.com>
2025-07-23 17:36:02 -0300

    Tradução do Menu Principal

--- Game_Project_Files/Assets/Script/MainMenu/ConfirmationPopupMenu.cs
+++ Game_Project_Files/Assets/Script/MainMenu/ConfirmationPopupMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using TMPro;
+using UnityEngine.Localization;
 
 public class ConfirmationPopupMenu : Menu
 {
@@ -12,12 +13,11 @@ public class ConfirmationPopupMenu : Menu
     [SerializeField] private Button confirmButton;
     [SerializeField] private Button cancelButton;
 
-    public void ActivateMenu(string displayText, UnityAction confirmAction, UnityAction cancelAction)
+    public void ActivateMenu(LocalizedString displayText, UnityAction confirmAction, UnityAction cancelAction)
     {
         this.gameObject.SetActive(true);
 
-        // set the display text
-        this.displayText.text = displayText;
+        displayText.StringChanged += UpdateText;
 
         // remove any existing listeners just to make sure there aren't any previous ones hanging around
         // note - this only removes listeners added through code
@@ -25,18 +25,28 @@ public class ConfirmationPopupMenu : Menu
         cancelButton.onClick.RemoveAllListeners();
 
         // assign the onClick listeners
-        confirmButton.onClick.AddListener(() => {
-            DeactivateMenu();
+        confirmButton.onClick.AddListener(() =>
+        {
+            DeactivateMenu(displayText);
             confirmAction();
         });
-        cancelButton.onClick.AddListener(() => {
-            DeactivateMenu();
+        cancelButton.onClick.AddListener(() =>
+        {
+            DeactivateMenu(displayText);
             cancelAction();
         });
     }
 
-    private void DeactivateMenu() 
+    private void DeactivateMenu(LocalizedString displayText)
     {
+        displayText.StringChanged -= UpdateText;
+        
         this.gameObject.SetActive(false);
+    }
+    
+    private void UpdateText(string value)
+    {
+        // set the display text
+        this.displayText.text = value;
     }
 }
--- Game_Project_Files/Assets/Script/MainMenu/SaveSlot.cs
+++ Game_Project_Files/Assets/Script/MainMenu/SaveSlot.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 using System;
 using UnityEngine.SceneManagement;
 using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEditor.Localization;
 
 public class SaveSlot : MonoBehaviour
 {
@@ -15,13 +17,18 @@ public class SaveSlot : MonoBehaviour
     [Header("Content")]
     [SerializeField] private GameObject noDataContent;
     [SerializeField] private GameObject hasDataContent;
-    [SerializeField] private TextMeshProUGUI percentageCompleteText;
-    [SerializeField] private TextMeshProUGUI deathCountText;
+    [SerializeField] private TextMeshProUGUI lastLocationTMPUGUI;
+    [SerializeField] private LocalizedString location_WordLocalizedString;
+    [SerializeField] private TextMeshProUGUI lastPlayedTMPUGUI;
+    [SerializeField] private LocalizedString lastPlayed_WordLocalizedString;
+    private string location_WordValue;
+    private LocalizedString currentSceneLocalizedString;
+    private string currentSceneString;
+    private string lastPlayedValue;
 
 
     [Header("Localized Strings for Scenes")]
-    [SerializeField] private LocalizedString _SampleSceneName;
-    [SerializeField] private LocalizedString _HallwayName;
+    [SerializeField] private List<LocalizedString> _InGameSceneNames;
 
     // [Header("Clear Data Button")]
     // [SerializeField] private Button clearButton;
@@ -34,7 +41,7 @@ public class SaveSlot : MonoBehaviour
     {
         saveSlotButton = this.GetComponent<Button>();
     }
-
+    
     public void SetData(GameData data)
     {
         // there's no data for this profileId
@@ -43,7 +50,6 @@ public class SaveSlot : MonoBehaviour
             hasData = false;
             noDataContent.SetActive(true);
             hasDataContent.SetActive(false);
-            // clearButton.gameObject.SetActive(false);
         }
         // there is data for this profileId
         else
@@ -51,14 +57,38 @@ public class SaveSlot : MonoBehaviour
             hasData = true;
             noDataContent.SetActive(false);
             hasDataContent.SetActive(true);
-            // clearButton.gameObject.SetActive(true);
 
-            // percentageCompleteText.text = data.GetPercentageComplete() + "% COMPLETE";
-            percentageCompleteText.text = "Location: " + data._currentScene;
+
+            currentSceneLocalizedString = null;
+            foreach (var ls in _InGameSceneNames)
+            {
+                var collection = LocalizationEditorSettings.GetStringTableCollection(ls.TableReference);
+                var entry = collection.SharedData.GetEntryFromReference(ls.TableEntryReference);
+                if (entry.Key == data._currentScene)
+                {
+                    currentSceneLocalizedString = ls;
+                    currentSceneString = ls.GetLocalizedString();
+                    break;
+                }
+
+            }
+            if (currentSceneLocalizedString == null)
+            {
+                Debug.LogWarning($"No localized string found for scene: {data._currentScene}");
+                currentSceneString = data._currentScene;
+            }
+
+            // Update the UI with the data
+            location_WordValue = location_WordLocalizedString.GetLocalizedString();
+            lastLocationTMPUGUI.text = location_WordValue + currentSceneString;
 
             long ticks = data.lastUpdated;
             DateTime dt = DateTime.FromBinary(ticks);
-            deathCountText.text = "LAST PLAYED: " + dt.ToLocalTime().ToString();
+            // lastPlayedTMPUGUI.text = "LAST PLAYED: " + dt.ToLocalTime().ToString();
+            lastPlayedValue = dt.ToLocalTime().ToString();
+
+            lastPlayedTMPUGUI.text = lastPlayed_WordLocalizedString.GetLocalizedString() + lastPlayedValue;
+            
         }
     }
 
@@ -70,7 +100,48 @@ public class SaveSlot : MonoBehaviour
     public void SetInteractable(bool interactable)
     {
         saveSlotButton.interactable = interactable;
-        // clearButton.interactable = interactable;
+    }
+
+    private void OnEnable()
+    {
+        location_WordLocalizedString.StringChanged += OnWordLocationChanged;
+        lastPlayed_WordLocalizedString.StringChanged += UpdateLastPlayed;
+        if (currentSceneLocalizedString != null)
+        {
+            currentSceneLocalizedString.StringChanged += OnLastSceneChanged;
+        }
+    }
+
+    private void OnDisable()
+    {
+        location_WordLocalizedString.StringChanged -= OnWordLocationChanged;
+        lastPlayed_WordLocalizedString.StringChanged -= UpdateLastPlayed;
+        if (currentSceneLocalizedString != null)
+        {
+            currentSceneLocalizedString.StringChanged -= OnLastSceneChanged;
+        }
+    }
+
+    private void OnWordLocationChanged(string value)
+    {
+        location_WordValue = value;
+        UpdateLocation();
+    }
+    private void OnLastSceneChanged(string value)
+    {
+        currentSceneString = value;
+        UpdateLocation();
+    }
+    private void UpdateLocation()
+    {
+        lastLocationTMPUGUI.text = location_WordValue + currentSceneString;
+    }
+
+
+
+    private void UpdateLastPlayed(string value)
+    {
+        lastPlayedTMPUGUI.text = value + lastPlayedValue;
     }
     
 }
