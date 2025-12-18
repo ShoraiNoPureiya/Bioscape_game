2731f550fd1d0e93135ad6c94d41516928c5202a
ShoraiNoPureiya <bycaferprohd2@gmail.com>
2025-12-18 12:56:08 -0300

    att

--- Game_Project_Files/Assets/Script/InteractSecondMission.cs
+++ Game_Project_Files/Assets/Script/InteractSecondMission.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
-public class InteractSecondMission : MonoBehaviour
+public class InteractSecondMission : MonoBehaviour, IDataPersistence
 {
     // Start is called before the first frame update
     public GameObject _Mission;
+    public bool _I;
     void Start()
     {
         _Mission.SetActive(true);
+        _I = true;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    public void LoadData(GameData data)
+    {
+        // Carrega os objetos existentes do save
+        _I = data._InteractSecondMission;
+    }
+
+    public void SaveData(GameData data)
+    {
+        // Salva todos os IDs registrados
+        data._InteractSecondMission = _I;
     }
 }
