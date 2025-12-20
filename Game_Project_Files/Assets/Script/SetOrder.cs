9410c4356e9bee4958d7f8b56649db9ec5ebb015
ShoraiNoPureiya <bycaferprohd2@gmail.com>
2025-12-19 21:12:59 -0300

    a

--- Game_Project_Files/Assets/Script/SetOrder.cs
+++ Game_Project_Files/Assets/Script/SetOrder.cs
@@ -29,7 +29,7 @@ public class SetOrder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(DataPersistenceManager.instance.CurrentOrder);
     }
 
 }
